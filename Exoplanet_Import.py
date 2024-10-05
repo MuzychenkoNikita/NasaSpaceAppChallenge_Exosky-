62d7d15064d6d34b579f68a58f77b695631a91fa
Advodkado <advodkado7@gmail.com>
2024-10-05 22:23:30 +0700

    Added Cartesian co-ordinates, added star radius

--- Exoplanet_Import.py
+++ Exoplanet_Import.py
@@ -1,7 +1,7 @@
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
-from pprint import pprint
+import math
 import numpy as np
 
 def Get_Exoplanets(amount = 1000):
@@ -10,10 +10,15 @@ def Get_Exoplanets(amount = 1000):
 
     output = []
     for row in r:
+        distance = np.float32((1000/row[3])*3.26156)
+        azimuth = (15 * int(row[1][:2]) + int(row[1][3:5])/4 + float(row[1][6:11])/240)*(math.pi/180)
+        elevation = (15 * int(row[2][:3]) + int(row[2][4:6])/4 + float(row[2][7:11])/240)*(math.pi/180)
+
         output.append({
             'planet_id': row[0],
-            'ra': np.float64(15 * int(row[1][:2]) + int(row[1][3:5])/4 + float(row[1][6:11])/240),
-            'dec': np.float64(15 * int(row[2][:3]) + int(row[2][4:6])/4 + float(row[2][7:11])/240),
+            'x': np.float64(distance*math.cos(elevation)*math.cos(azimuth)),
+            'y': np.float64(distance*math.cos(elevation)*math.sin(azimuth)),
+            'z': np.float64(distance*math.sin(elevation)),
             'dist': np.float32(row[3] * 3.26156)
             })
 
--- Star_Import.py
+++ Star_Import.py
@@ -2,14 +2,21 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astroquery.gaia import Gaia
 import numpy as np
+import math
 
 def Get_Stars(amount = 1000):
     query = f"""
     SELECT TOP {amount}
-        source_id, ra, dec, phot_g_mean_mag, parallax
-    FROM gaiadr3.gaia_source
-    WHERE parallax_error / parallax < 0.2
-    ORDER BY phot_g_mean_mag ASC
+        gaiadr3.gaia_source.source_id, 
+        gaiadr3.gaia_source.ra, 
+        gaiadr3.gaia_source.dec, 
+        gaiadr3.gaia_source.phot_g_mean_mag, 
+        gaiadr3.gaia_source.parallax, 
+        gaiadr2.gaia_source.radius_val 
+    FROM gaiadr3.gaia_source 
+    LEFT JOIN gaiadr2.gaia_source ON gaiadr3.gaia_source.source_id = gaiadr2.gaia_source.source_id
+    WHERE gaiadr3.gaia_source.parallax_error / gaiadr3.gaia_source.parallax < 0.2
+    ORDER BY gaiadr3.gaia_source.phot_g_mean_mag ASC
     """
 
     job = Gaia.launch_job(query)
@@ -17,12 +24,18 @@ def Get_Stars(amount = 1000):
 
     output = []
     for row in r:
+        distance = np.float32((1000/row[4])*3.26156)
+        azimuth = row[1]*(math.pi/180)
+        elevation = row[2]*(math.pi/180)
+
         output.append({
             'source_id': row[0],
-            'ra': row[1],
-            'dec': row[2],
+            'x': np.float64(distance*math.cos(elevation)*math.cos(azimuth)),
+            'y': np.float64(distance*math.cos(elevation)*math.sin(azimuth)),
+            'z': np.float64(distance*math.sin(elevation)),
             'g_mag': row[3],
-            'dist': np.float32((1000/row[4])*3.26156)
+            'dist': distance,
+            'rad': row[4] * 7.35355*(10**-8)
             })
 
     for i in output:
