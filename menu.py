9654add041e2fba5325d3e4851dda9f18876a90e
MuzychenkoNikita <nik.muzychenko@gmail.com>
2024-10-06 19:04:28 +0700

    minor change

--- menu.py
+++ menu.py
@@ -16,9 +16,9 @@ def menu():
 def photo_button():
 
     imgui.set_next_window_size(100, 100)
-    # imgui.set_next_window_position(0, 0)
+    imgui.set_next_window_position(1000, 0)
     imgui.begin(
-        "",
+        "Photo",
         flags=imgui.WINDOW_NO_MOVE | imgui.WINDOW_NO_RESIZE
     )
     imgui.button("click me!", width=100, height=100)
